c7387cc997ba063645a865af63da5c1d50f9625a
araae <araliae@icloud.com>
2026-05-25 13:06:31 -0700

    updated main and app

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
+//import Routes, Route, login and register
+import { Routes, Route } from "react-router";
+import Login from "./Login";
+import Register from "./Register";
+
+//define all routes
 export default function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { BrowserRouter } from "react-router";
+import { AuthProvider } from "./AuthContext";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <AuthProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </AuthProvider>,
 );
+
+//updated with browserRouter and AuthProvider
